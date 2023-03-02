129f77d70e579b9605a706616a6353e60baf1586
Monika Gorkani <monika.gorkani@chargepoint.com>
2023-03-01 20:47:56 -0800

    session1 stuff

---
+++ iOSStudyCircle/session1/Hello SwiftUI/Hello SwiftUI/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Hello SwiftUI
+//
+//  Created by Monika Gorkani on 1/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var name = ""
+    var body: some View {
+        VStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                TextField("Enter your name", text: $name)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .border(Color.red, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .padding(.all)
+                    
+                Text(name.count > 0 ? "Hello, \(name)!" : "Hello")
+                    .font(.title)
+                
+                Button("Reset name") {
+                    name = ""
+                }
+                .padding(.all)
+                
+            }
+            
+            .padding()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
