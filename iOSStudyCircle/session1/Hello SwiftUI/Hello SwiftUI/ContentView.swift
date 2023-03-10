a590d140deb43bda301443ba44efa57156d5753c
Monika Gorkani <monika.gorkani@chargepoint.com>
2023-03-10 14:46:48 -0800

    updated exercise based on feedback today

--- iOSStudyCircle/session1/Hello SwiftUI/Hello SwiftUI/ContentView.swift
+++ iOSStudyCircle/session1/Hello SwiftUI/Hello SwiftUI/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     @State var name = ""
     var body: some View {
-        VStack {
+        
             VStack {
                 Image(systemName: "globe")
                     .imageScale(.large)
                     .foregroundColor(.accentColor)
-                TextField("Enter your name", text: $name)
+                TextField("Enter your name", text:$name)
                     .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     .border(Color.red, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                     .padding(.all)
                     
-                Text(name.count > 0 ? "Hello, \(name)!" : "Hello")
+                Text(!name.isEmpty ? "Hello, \(name)!" : "Hello")
                     .font(.title)
                 
                 Button("Reset name") {
@@ -30,9 +30,7 @@ struct ContentView: View {
                 
             }
             
-            .padding()
-            
-        }
+       
     }
 }
 
