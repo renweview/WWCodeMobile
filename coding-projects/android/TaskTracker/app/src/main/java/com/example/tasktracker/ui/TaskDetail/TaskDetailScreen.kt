d0fc00e64df91fce6772d797cb1a70499263b5ec
Gauri Gadkari <gauri.gadkari@gmail.com>
2024-01-23 23:39:16 -0800

    initial commit for project structure

---
+++ coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/ui/TaskDetail/TaskDetailScreen.kt
@@ -0,0 +1,8 @@
+package com.example.tasktracker.ui.TaskDetail
+
+/**
+ * Created by Gauri Gadkari on 1/23/24.
+ * Screen to display task details and add, edit or delete task
+ */
+class TaskDetailScreen {
+}
