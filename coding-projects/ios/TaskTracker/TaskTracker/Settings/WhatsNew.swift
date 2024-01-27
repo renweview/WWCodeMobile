68cc699edb6bb816268d040041f8818b49bbf0b8
Claudia Maciel <clc_80@yahoo.com>
2024-01-27 09:34:50 -0800

    Added Setting files for beginner issues

---
+++ coding-projects/ios/TaskTracker/TaskTracker/Settings/WhatsNew.swift
@@ -0,0 +1,18 @@
+//
+//  WhatsNew.swift
+//  TaskTracker
+//
+//  Created by Claudia Maciel on 1/27/24.
+//
+
+import SwiftUI
+
+struct WhatsNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhatsNew()
+}
