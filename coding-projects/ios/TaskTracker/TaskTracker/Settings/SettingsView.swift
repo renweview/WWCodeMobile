ffe3c5821365044c775cc29e8cb653b67a2cebdc
Claudia Maciel <clc_80@yahoo.com>
2024-01-29 18:11:57 -0800

    Added issue number to TODO

--- coding-projects/ios/TaskTracker/TaskTracker/Settings/SettingsView.swift
+++ coding-projects/ios/TaskTracker/TaskTracker/Settings/SettingsView.swift
@@ -11,39 +11,39 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             Form {
-                // TODO: Add Section Header: Info and Feedback
-                // TODO: Add version number footer
+                // TODO: Add Section Header: Info and Feedback #105
+                // TODO: Add version number footer #122
                 Section {
-                    // TODO: Add About us row
-                    // TODO: Add Privacy Policy row
-                    // TODO: Add Tutorial row
-                    // TODO: Add Rate App row
-                    // TODO: Add follow us on twitter row
+                    // TODO: Add About us row #109
+                    // TODO: Add Privacy Policy row #110
+                    // TODO: Add Tutorial row #111
+                    // TODO: Add Rate App row #112
+                    // TODO: Add follow us on twitter row #113
                 }
 
-                // TODO: Add Section Header: Notifications
+                // TODO: Add Section Header: Notifications #106
                 Section {
-                    // TODO: Add Days with Day bubbles
-                    // TODO: Add Task Reminder Row (with toggle)
-                    // TODO: Add Show Badge Row (with toggle)
-                    // TODO: Add Reminder Time
+                    // TODO: Add Days with Day bubbles #120
+                    // TODO: Add Task Reminder Row (with toggle) #118
+                    // TODO: Add Show Badge Row (with toggle) #119
+                    // TODO: Add Reminder Time #121
                 }
 
-                // TODO: Add Section Header: What's New
+                // TODO: Add Section Header: What's New #107
                 Section {
-                    // TODO: Add version 1.0 row
-                    // TODO: Add vote on future requests row
+                    // TODO: Add version 1.0 row #114
+                    // TODO: Add vote on future requests row #115
                 }
 
-                // TODO: Add Section Header: Appearance
+                // TODO: Add Section Header: Appearance #108
                 Section {
-                    // TODO: Add App Theme Row
-                    // TODO: App Icon
+                    // TODO: Add App Theme Row #116
+                    // TODO: App Icon #117
                 }
 
             }
         }
-        // TODO: Add navigation title to settings view
+        // TODO: Add navigation title to settings view #104
     }
 }
 
