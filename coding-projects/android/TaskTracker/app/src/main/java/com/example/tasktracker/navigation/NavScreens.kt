1df122750c18a587f0c4426af80fc54bcba88d76
Baghaii <baghaii@gmail.com>
2024-02-04 16:47:07 -0700

    Move strings to companion object

--- coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/navigation/NavScreens.kt
+++ coding-projects/android/TaskTracker/app/src/main/java/com/example/tasktracker/navigation/NavScreens.kt
@@ -1,7 +1,14 @@
 package com.example.tasktracker.navigation
 
+import androidx.compose.runtime.Composable
+
 sealed class NavScreens(val route:String) {
-    object TaskList: NavScreens("List")
-    object TaskSettings: NavScreens("Settings")
-    object TaskDetail: NavScreens("Detail")
+    companion object {
+        private const val LIST_SCREEN_ROUTE = "List"
+        private const val SETTINGS_SCREEN_ROUTE = "Settings"
+        private const val DETAIL_SCREEN_ROUTE = "Detail"
+    }
+    object TaskList: NavScreens(LIST_SCREEN_ROUTE)
+    object TaskSettings: NavScreens(SETTINGS_SCREEN_ROUTE)
+    object TaskDetail: NavScreens(DETAIL_SCREEN_ROUTE)
 }
